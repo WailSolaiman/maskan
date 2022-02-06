1b15ba454c388018ec1406476a8e9b0564ffa751
Wail Solaiman <wailsolaiman@hotmail.com>
2022-02-06 02:27:27 +0100

    update headers in fetchApi

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -6,7 +6,7 @@ export const fetchApi = async (url) => {
   const { data } = await axios.get(url, {
     headers: {
       'x-rapidapi-host': 'bayut.p.rapidapi.com',
-      'x-rapidapi-key': '2165a6d469msh5e8c16f84ee6c79p1ad00cjsn57b2695d06fe',
+      'x-rapidapi-key': 'a054f306camsh8cff6bc0c25e37ap168447jsn00438a179c9f',
     },
   })
   return data
