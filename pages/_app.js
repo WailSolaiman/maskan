b7c6a6e49df1b124be15f6e4137f12816af740bd
Wail Solaiman <wailsolaiman@hotmail.com>
2022-02-06 02:02:16 +0100

    update app to use chackra ui provider and to configure loading progress

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,35 @@
-import '../styles/globals.css'
+import Head from 'next/head'
+import Router from 'next/router'
+import NProgress from 'nprogress'
+import { ChakraProvider } from '@chakra-ui/react'
+
+import Layout from '../components/Layout'
+import '../styles/nprogress.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  NProgress.configure({ showSpinner: false })
+
+  Router.events.on('routeChangeStart', () => {
+    NProgress.start()
+  })
+
+  Router.events.on('routeChangeComplete', () => {
+    NProgress.done()
+  })
+
+  return (
+    <>
+      <Head>
+        <title>Maskan - Real Estate</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <ChakraProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ChakraProvider>
+    </>
+  )
 }
 
 export default MyApp
