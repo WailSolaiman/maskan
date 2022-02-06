11cf2dd67957afca87102930748b1a3f6b11cc1e
Wail Solaiman <wailsolaiman@hotmail.com>
2022-02-06 02:03:10 +0100

    add/update various root files

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   reactStrictMode: true,
+  eslint: {
+    dirs: ['pages', 'utils'], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
+  },
+  images: {
+    domains: ['bayut-production.s3.eu-central-1.amazonaws.com']
+  }
 }
